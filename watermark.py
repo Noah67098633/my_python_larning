a2ef989e526eceef7a7caf8e8051e7471a7395c7
Noah Guo <zhiquan_guo@cathaypacific.com>
2022-06-11 13:48:57 +0800

    change to square mode

--- watermark.py
+++ watermark.py
@@ -4,11 +4,11 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 import piexif
 
-BORDER = 60
-LONGEDGE = 2000 - BORDER * 2
-EXIFINFOSIZE = 20
-COPYRIGHTSIZE = 40
-TEXTINFOH = BORDER - (0.04 * BORDER)
+BORDER = 240
+OUTPUTWIDTH = 3000
+TEXTPADDING = 20
+EXIFINFOSIZE = 40
+COPYRIGHTSIZE = 60
 EXIFFONT = "Arial"
 COPYRIGHTFONT = "Arial"
 TARGETPATH = "/Users/Noah/Pictures/watermark/"
@@ -27,36 +27,43 @@ def makeWatermark(imagePath):
     
 
     (originw, originh) = image.size
-    longEdge = 0
-    shortEdge = 0
-    islansacape = True
+    imageX = 0
+    imageY = 0
+    imageWidth = 0
+    imageHeigh = 0
     if originw > originh:
-        longEdge = originw
-        shortEdge = originh
+        radio = originh / originw
+        imageWidth = int(OUTPUTWIDTH - BORDER * 2)
+        imageHeigh = int(imageWidth * radio)
+        imageX = BORDER
+        imageY = int((OUTPUTWIDTH - imageHeigh)  / 2)
+    
     else:
-        longEdge = originh
-        shortEdge = originw
-        islansacape = False
+        radio = originw / originh
+        imageHeigh = int(OUTPUTWIDTH - BORDER * 2)
+        imageWidth = int(imageHeigh * radio)
+        imageY = BORDER
+        imageX = int((OUTPUTWIDTH - imageWidth)  / 2)
 
-    radio = float(LONGEDGE / longEdge)
-    width = int(LONGEDGE if islansacape else shortEdge * radio)
-    heigh = int(LONGEDGE if not islansacape else shortEdge * radio)
-    image = image.resize((width, heigh), Image.LANCZOS)
+
+
+    image = image.resize((imageWidth, imageHeigh), Image.LANCZOS)
 
     combineImage = Image.new(
-        'RGB', (width + BORDER * 2, heigh + BORDER * 2), (255, 255, 255))
-    combineImage.paste(image, (BORDER, BORDER))
+        'RGB', (OUTPUTWIDTH, OUTPUTWIDTH), (255, 255, 255))
+    combineImage.paste(image, (imageX, imageY))
 
     exifInfo = makeExifInfoText(imagePath)
     
     font = ImageFont.truetype(font=EXIFFONT, size=EXIFINFOSIZE)
     font2 = ImageFont.truetype(font=COPYRIGHTFONT, size=COPYRIGHTSIZE)
 
+    textH = imageY + imageHeigh + TEXTPADDING
     draw = ImageDraw.Draw(combineImage)
-    draw.text(xy=(BORDER, combineImage.height-TEXTINFOH),
+    draw.text(xy=(imageX, textH),
               text=exifInfo, fill=(0, 0, 0), font=font)
     textSize = draw.textsize(text=COPYRIGHT, font=font2,)
-    draw.text(xy=(combineImage.width - textSize[0] - BORDER, combineImage.height-TEXTINFOH),
+    draw.text(xy=((imageWidth + imageX) - textSize[0], textH),
               text=COPYRIGHT, fill=(0, 0, 0), font=font2)
     exifDic["Exif"][piexif.ExifIFD.PixelXDimension] = combineImage.width
     exifDic["Exif"][piexif.ExifIFD.PixelYDimension] = combineImage.height
