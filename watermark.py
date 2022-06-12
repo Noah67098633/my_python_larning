9565780250226dc17fbfbe81d6d5adef070bf9d8
Noah Guo <zhiquan_guo@cathaypacific.com>
2022-06-12 23:33:40 +0800

    added logo feature

--- watermark.py
+++ watermark.py
@@ -1,19 +1,26 @@
 from datetime import datetime
 from sys import argv
 import os
+import sys
 from PIL import Image, ImageDraw, ImageFont
 import piexif
 
-BORDER = 120
+BORDER = 140
+LOGO_HEIGH = int(BORDER / 1.8)
 CONTAINER_LONG_EDGE = 3000
-CONTAINER_SHORT_EDGE = 2000
-TEXT_PADDING = 10
-EXIF_INFO_SIZE = 30
-COPYRIGHT_SIZE = 40
-EXIF_FONT = "Arial"
-COPYRIGHT_FONT = "Arial"
+CONTAINER_SHORT_EDGE = int(CONTAINER_LONG_EDGE * (2/3))
+TEXT_PADDING = 8
+EXIF_INFO_SIZE = 26
+COPYRIGHT_SIZE = 36
+FONT_COLOR = (60,60,60)
+EXIF_FONT = "Arial Bold.ttf"
+COPYRIGHT_FONT = EXIF_FONT
 TARGET_PATH = "/Users/Noah/Pictures/watermark/"
 COPYRIGHT = f"©Noah {datetime.now().year}"
+
+
+LOGO_MAPPING = {"NIKON CORPORATION": f"{sys.path[0]}/logo/nikon_logo.jpg",
+                "FUJIFILM": f"{sys.path[0]}/logo/fuji_logo.jpg"}
 
 
 def makeWatermark(imagePath):
@@ -25,14 +32,18 @@ def makeWatermark(imagePath):
     target = f"{TARGET_PATH}{fileName}"
     image = Image.open(imagePath)
     exifDic = piexif.load(image.info["exif"])
-    
+
     (originw, originh) = image.size
+
     containerImageW = CONTAINER_LONG_EDGE if originw > originh else CONTAINER_SHORT_EDGE
-    containerImageH = CONTAINER_LONG_EDGE if originw < originh else CONTAINER_SHORT_EDGE
+    containerImageH = CONTAINER_LONG_EDGE if originw <= originh else CONTAINER_SHORT_EDGE
+    radio = (containerImageW - BORDER * 2) / containerImageW if originw > originh else (
+        containerImageH - BORDER * 2) / containerImageH
+    scale = min((containerImageW * radio) / originw,
+                (containerImageH * radio) / originh)
 
-    scale = min((containerImageW - BORDER * 2) / originw, (containerImageH - BORDER * 2) / originh)
-
-    image = image.resize((int(originw * scale), int(originh * scale)), Image.LANCZOS)
+    image = image.resize(
+        (int(originw * scale), int(originh * scale)), Image.LANCZOS)
 
     imageX = int((containerImageW - image.width) / 2)
     imageY = int((containerImageH - image.height) / 2)
@@ -45,21 +56,33 @@ def makeWatermark(imagePath):
     containerImage.paste(image, (imageX, imageY))
 
     exifInfo = makeExifInfoText(imagePath)
-    
+
     font = ImageFont.truetype(font=EXIF_FONT, size=EXIF_INFO_SIZE)
     font2 = ImageFont.truetype(font=COPYRIGHT_FONT, size=COPYRIGHT_SIZE)
 
     textH = imageY + image.height + TEXT_PADDING
     draw = ImageDraw.Draw(containerImage)
     draw.text(xy=(imageX, textH),
-              text=exifInfo, fill=(0, 0, 0), font=font)
+              text=exifInfo, fill=FONT_COLOR, font=font)
     textSize = draw.textsize(text=COPYRIGHT, font=font2)
     draw.text(xy=((image.width + imageX) - textSize[0], textH),
-              text=COPYRIGHT, fill=(0, 0, 0), font=font2)
+              text=COPYRIGHT, fill=FONT_COLOR, font=font2)
+
+    make = str(exifDic["0th"][piexif.ImageIFD.Make], encoding="utf-8")
+    logoPath = LOGO_MAPPING[make]
+    if  logoPath and os.path.exists(logoPath):
+        logo = Image.open(logoPath)
+        logoRadio = logo.width / logo.height
+        logo = logo.resize((int(LOGO_HEIGH * logoRadio), LOGO_HEIGH))
+        x = int((containerImage.width - logo.width) / 2)
+        y = imageY + image.height
+        containerImage.paste(logo, (x, y))
+
     exifDic["Exif"][piexif.ExifIFD.PixelXDimension] = containerImage.width
     exifDic["Exif"][piexif.ExifIFD.PixelYDimension] = containerImage.height
     exifDic["thumbnail"] = None
     exifBytes = piexif.dump(exifDic)
+
     containerImage.save(target, quality=99, subsampling=0, exif=exifBytes)
 
     print(f"{imagePath} done")
@@ -79,10 +102,13 @@ def makeExifInfoText(imagePath):
     model = str(newDic.get("Model", "-"), encoding="utf-8")
     make = str(newDic.get("Make", "-"), encoding="utf-8")
     lens = str(newDic.get("LensModel", "-"), encoding="utf-8")
-    cameraInfo = f"{make} {model} {lens}"
+    if "x100" in model.lower():
+        lens = ""
+    cameraInfo = f"{make} {model} {lens}".replace(
+        "NIKON CORPORATION NIKON", "NIKON")
     FStop = newDic.get("FNumber", "-")
     FStopNum = str(float(FStop[0]) / float(FStop[1]
-                                       )).replace(".0", "") if len(FStop) == 2 else FStop[0]
+                                           )).replace(".0", "") if len(FStop) == 2 else FStop[0]
 
     shutterTime = newDic.get("ExposureTime", "0")
     shutterTimeNum = f"{shutterTime[0]}/{shutterTime[1]}" if len(
@@ -111,4 +137,3 @@ if __name__ == '__main__':
         makeWatermark(image)
     end = datetime.now()
     print(end - start)
-
